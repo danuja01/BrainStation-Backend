564d789bfa1a67d5ad1acf79cd13eeb269079663
danuja01 <it21023378@my.sliit.lk>
2024-09-09 12:37:55 +0530

    Feat: Impliment forecast APIs

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import analyticsRouter from './analytics.routes';
 import authRouter from './auth.routes';
+import forcastRouter from './forcast.routes';
 import questionRouter from './question.routes';
 import quizRouter from './quiz.routes';
 import userRouter from './user.routes';
@@ -13,5 +14,6 @@ router.use('/users', protect, userRouter);
 router.use('/questions', protect, questionRouter);
 router.use('/quizzes', protect, quizRouter);
 router.use('/analytics', protect, analyticsRouter);
+router.use('/forecast', protect, forcastRouter);
 
 export default router;
