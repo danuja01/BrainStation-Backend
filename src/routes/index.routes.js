16831685d13c033201ceb63168ed7c9d6b858ff8
Supun Kariyawasam <bhasura2105@gmail.com>
2024-09-09 23:39:48 +0530

    Feat: Implement assrs and session api end points

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import analyticsRouter from './analytics.routes';
+import assrsResultRouter from './assrsResult.routes';
 import authRouter from './auth.routes';
 import lectureRouter from './lecture.routes';
 import moduleRouter from './module.routes';
 import ontologyRouter from './ontology.routes';
 import questionRouter from './question.routes';
 import quizRouter from './quiz.routes';
+import sessionRouter from './session.routes';
 import userRouter from './user.routes';
 import { protect } from '@/middleware';
 
@@ -19,5 +21,7 @@ router.use('/analytics', protect, analyticsRouter);
 router.use('/ontology', ontologyRouter);
 router.use('/lectures', lectureRouter);
 router.use('/modules', moduleRouter);
+router.use('/assrs', assrsResultRouter);
+router.use('/sessions', sessionRouter);
 
 export default router;
