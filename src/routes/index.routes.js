ae488b20b1f4e7e5327434bfcafefd7f259896cc
Supun Kariyawasam <bhasura2105@gmail.com>
2024-10-26 16:57:18 +0530

    Refactor: Added authorizor to the session routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { protect } from '@/middleware';
 import algorithmRouter from './algorithm.routes';
 import analyticsRouter from './analytics.routes';
 import assrsResultRouter from './assrsResult.routes';
@@ -13,7 +14,6 @@ import quizRouter from './quiz.routes';
 import sessionRouter from './record.routes';
 import taskRouter from './task.routes';
 import userRouter from './user.routes';
-import { protect } from '@/middleware';
 
 const router = express.Router();
 
@@ -27,7 +27,7 @@ router.use('/ontology', protect, ontologyRouter);
 router.use('/lectures', protect, lectureRouter);
 router.use('/modules', moduleRouter);
 router.use('/assrs', protect, assrsResultRouter);
-router.use('/sessions', sessionRouter);
+router.use('/sessions', protect, sessionRouter);
 router.use('/progress', protect, progressRouter);
 router.use('/algorithm', algorithmRouter);
 router.use('/task', protect, taskRouter);
--- src/routes/record.routes.js
+++ src/routes/record.routes.js
@@ -13,69 +13,82 @@ import {
   getTotalFocusTimeOfUsersModuleController,
   getTotalSessionDurationByUserController
 } from '@/controllers/session';
+import { authorizer } from '@/middleware';
 import { focusRecordIdSchema } from '@/validations/focusRecords';
 
 const sessionRouter = express.Router();
 
 // Route to create a new session
-sessionRouter.post('/', tracedAsyncHandler(addSessionController));
+sessionRouter.post('/', authorizer(['STUDENT', 'LECTURER', 'ADMIN']), tracedAsyncHandler(addSessionController));
 
 // Route to get a session by ID
-sessionRouter.get('/:id', tracedAsyncHandler(getSessionByIdController));
+sessionRouter.get('/:id', authorizer(['STUDENT', 'LECTURER', 'ADMIN']), tracedAsyncHandler(getSessionByIdController));
 
 // Route to get all sessions by user ID
-sessionRouter.get('/user/:userId', tracedAsyncHandler(getSessionByUserController));
+sessionRouter.get(
+  '/user',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
+  tracedAsyncHandler(getSessionByUserController)
+);
 
 // Route to get all sessions by users
 sessionRouter.get(
-  '/userByModule/:userId',
+  '/userByModule',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getSessionsOfUserByModuleController)
 );
 
 // Route to get user start end times per module
 sessionRouter.get(
-  '/start-end-times/:userId',
+  '/start-end-times',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getStartAndEndTimesOfUsersModuleController)
 );
 
 // Route to get user total focus time per module
 sessionRouter.get(
-  '/total-focus-time/:userId',
+  '/total-focus-time',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getTotalFocusTimeOfUsersModuleController)
 );
 
 // Route to get user avarage focus time per module
 sessionRouter.get(
-  '/average-focus-time/:userId',
+  '/average-focus-time',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getAverageFocusTimeofUsersModuleController)
 );
 // Route to get all sessions by users
 sessionRouter.get(
-  '/userByModule/:userId',
+  '/userByModule',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getSessionsOfUserByModuleController)
 );
 
 sessionRouter.get(
-  '/sessionData/:userId',
+  '/sessionData',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getSessionDataController)
 );
 
 // Route to get user average focus time (without moduleId)
 sessionRouter.get(
-  '/average-focus-time-by-user/:userId',
+  '/average-focus-time-by-user',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getAverageFocusTimeByUserController)
 );
 
 // Route to get user total session duration by userId (sum of all session durations)
 sessionRouter.get(
-  '/total-session-duration-by-user/:userId',
+  '/total-session-duration-by-user',
+  authorizer(['STUDENT', 'LECTURER', 'ADMIN']),
   celebrate({ [Segments.PARAMS]: focusRecordIdSchema }),
   tracedAsyncHandler(getTotalSessionDurationByUserController)
 );
