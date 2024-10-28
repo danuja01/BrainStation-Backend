321646b577443a17b7ff3151e9fee9f240877cce
Supun Kariyawasam <bhasura2105@gmail.com>
2024-10-29 00:52:16 +0530

    Refactor: replace axios calls with variables

--- src/controllers/lecture.js
+++ src/controllers/lecture.js
@@ -23,8 +23,8 @@ export const createLecture = async (req, res) => {
       filename: filename,
       contentType: mimetype
     });
-
-    const response = await axios.post('http://34.30.64.175:9000/upload/', formData, {
+    console.log(process.env.LECTURE_URL);
+    const response = await axios.post(`${process.env.LECTURE_URL}upload/`, formData, {
       headers: {
         ...formData.getHeaders()
       }
